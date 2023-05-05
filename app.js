e3ae093f71059fdf7c0f5e5b50a09946e58322f3
Yagi2023 <132663410+Yagi2023@users.noreply.github.com>
2023-05-05 08:12:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,23 +6,28 @@ function showShipmentInfo() {
   // Get the tracking number value
   const trackingNumber = trackingNumberInput.value;
 
-  // Get the shipment information from Shippo's API
-  // Replace "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8" with your own Shippo API key
-  const apiKey = "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8";
-  const url = `https://api.goshippo.com/tracks/${trackingNumber}/`;
+  // Check if the tracking number is valid
+  if (trackingNumber) {
+    // Get the shipment information from Shippo's API
+    // Replace "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8" with your own Shippo API key
+    const apiKey = "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8";
+    const url = `https://api.goshippo.com/tracks/${trackingNumber}/`;
 
-  fetch(url, {
-    headers: {
-      "Authorization": `ShippoToken ${apiKey}`
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Display the shipment information
-      const shipmentInfo = document.getElementById("shipment-info");
-      shipmentInfo.innerHTML = JSON.stringify(data, null, 2);
+    fetch(url, {
+      headers: {
+        "Authorization": `ShippoToken ${apiKey}`
+      }
     })
-    .catch(error => {
-      console.error(error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        // Display the shipment information
+        const shipmentInfo = document.getElementById("shipment-info");
+        shipmentInfo.innerHTML = JSON.stringify(data, null, 2);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  } else {
+    alert("Please enter a valid tracking number.");
+  }
 }
