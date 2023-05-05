99e5f0d7ad7eaf57f66d735bc510c210a7210149
Yagi2023 <132663410+Yagi2023@users.noreply.github.com>
2023-05-05 08:56:23 -0400

    Update App.js

--- .github/workflows/App.js
+++ .github/workflows/App.js
@@ -1,12 +1,18 @@
-// Function to show the shipment information
-function showShipmentInfo() {
-  // Get the tracking number input
-  const trackingNumberInput = document.getElementById("tracking-number");
+// Function to show the admin portal
+function showAdminPortal() {
+  var adminPassword = document.getElementById("admin-password").value;
+  var adminPortal = document.getElementById("admin-portal");
+  if (adminPassword === "yagi3212!") {
+    adminPortal.style.display = "block";
+    adminPortal.classList.add("fadeIn");
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+}
 
-  // Get the tracking number value
-  const trackingNumber = trackingNumberInput.value;
-
-  // Get the shipment information from Shippo's API
+// Function to fetch and display shipment information
+function trackShipment() {
+  var trackingNumber = document.getElementById("tracking-number").value;
   // Replace "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8" with your own Shippo API key
   const apiKey = "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8";
   const url = `https://api.goshippo.com/tracks/${trackingNumber}/`;
@@ -26,3 +32,9 @@ function showShipmentInfo() {
       console.error(error);
     });
 }
+
+// Listen for form submission to track the shipment
+document.getElementById("tracking-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  trackShipment();
+});
