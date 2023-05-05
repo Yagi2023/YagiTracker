d3b41ceef422cfbbfaa058fe57b4ff43225c5559
Yagi2023 <132663410+Yagi2023@users.noreply.github.com>
2023-05-05 08:22:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-// Function to show the shipment information
-function showShipmentInfo() {
+// Function to show the shipment tracking interface
+function showShipmentTrackingInterface() {
+  // Get the holographic button element
+  const holographicButton = document.getElementById("holographic-button");
+
+  // Hide the holographic button
+  holographicButton.style.display = "none";
+
+  // Show the shipment tracking interface
+  document.getElementById("shipment-tracking-interface").style.display = "block";
+}
+
+// Function to track shipment information
+function trackShipment() {
   // Get the tracking number input
   const trackingNumberInput = document.getElementById("tracking-number");
 
   // Get the tracking number value
   const trackingNumber = trackingNumberInput.value;
 
-  // Check if the tracking number is valid
-  if (trackingNumber) {
+  // Check if the tracking number is a valid password
+  if (trackingNumber === "yagitracking") {
+    // Show the admin design screen
+    document.getElementById("admin-design-screen").style.display = "block";
+  } else {
+    // Hide the admin design screen
+    document.getElementById("admin-design-screen").style.display = "none";
+
     // Get the shipment information from Shippo's API
     // Replace "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8" with your own Shippo API key
     const apiKey = "shippo_live_3ba0f351f5fd23633a365c872586f52f42e103b8";
@@ -27,7 +45,5 @@ function showShipmentInfo() {
       .catch(error => {
         console.error(error);
       });
-  } else {
-    alert("Please enter a valid tracking number.");
   }
 }
